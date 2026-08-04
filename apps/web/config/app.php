58103c574a726da20582f56ff807d9929081f56f
ecreation48 <68565910+ecreation48@users.noreply.github.com>
2026-08-04 21:27:48 +0200

    Update app.php
    add app url for other 443 port

--- apps/web/config/app.php
+++ apps/web/config/app.php
@@ -1,2 +1,2 @@
 <?php
-return ['name'=>env('APP_NAME','Voice Guardian'),'env'=>env('APP_ENV','production'),'debug'=>(bool)env('APP_DEBUG',false),'url'=>env('APP_URL','http://localhost'),'timezone'=>'Europe/Paris','locale'=>'fr','fallback_locale'=>'en','key'=>env('APP_KEY'),'cipher'=>'AES-256-CBC'];
+return ['name'=>env('APP_NAME','Voice Guardian'),'env'=>env('APP_ENV','production'),'debug'=>(bool)env('APP_DEBUG',false),'url'=>env('APP_URL','http://localhost'),'asset_url' => env('ASSET_URL'),'timezone'=>'Europe/Paris','locale'=>'fr','fallback_locale'=>'en','key'=>env('APP_KEY'),'cipher'=>'AES-256-CBC'];
